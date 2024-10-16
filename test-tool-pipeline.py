b79e6c661e0c9cbe2c722d0b3751f1e266a74ab8
Leo-LS-web <82956431+Leo-LS-web@users.noreply.github.com>
2024-10-16 15:47:43 +0200

    test-tool-pipeline.py

---
+++ test-tool-pipeline.py
@@ -0,0 +1,46 @@
+"""
+requirements: blueprints
+"""
+
+import os
+import requests
+from typing import Literal, List, Optional
+from datetime import datetime
+
+
+from blueprints.function_calling_blueprint import Pipeline as FunctionCallingBlueprint
+
+
+class Pipeline(FunctionCallingBlueprint):
+    class Valves(FunctionCallingBlueprint.Valves):
+        # Add your custom parameters/configuration here e.g. API_KEY that you want user to configure etc.
+        pass
+
+    class Tools:
+        def __init__(self, pipeline) -> None:
+            self.pipeline = pipeline
+
+        def calculator(self, equation: str) -> str:
+            """
+            Calculate the result of an equation.
+
+            :param equation: The equation to calculate.
+            """
+
+            try:
+                result = eval(equation)
+                return f"{equation} = {result}"
+            except Exception as e:
+                print(e)
+                return "Invalid equation"
+
+    def __init__(self):
+        super().__init__()
+        self.name = "My Calculator Tool Pipeline"
+        self.valves = self.Valves(
+            **{
+                **self.valves.model_dump(),
+                "pipelines": ["*"],  # Connect to all pipelines
+            },
+        )
+        self.tools = self.Tools(self)
